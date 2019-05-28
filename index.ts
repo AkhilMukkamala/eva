3d43db8ea21794ba08fe377e75ba49ddd6de9248
Artur Yorsh <artyorsh.me@gmail.com>
2019-05-28 19:36:45 +0300

    refactor(package): rename

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-import { default as mapping } from './mapping.json';
-
-export default mapping;
+export { default as mapping } from './mapping.json';
+export { default as light } from './themes/light.json';
+export { default as dark } from './themes/dark.json';
